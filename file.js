c2d3e4e2c2265f86f33053c6c129f5d7bac0556c
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-06-19 18:19:29 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,17 @@
+import {createCipheriv} from 'node:crypto'
+function encryptValue(originalString) {
+  const encryptionKey = 'testkeys'
+  const bufferKey = Buffer.from(encryptionKey, 'utf8')
+  let cipher = createCipheriv('DES-ECB', bufferKey, null)
+  cipher.setAutoPadding(false)
+  let bufferString = Buffer.from(originalString, 'utf8')
+  let paddingSize = bufferKey.length - (bufferString.length % bufferKey.length)
+  let paddedStr = Buffer.concat([
+    bufferString,
+    Buffer.alloc(paddingSize, 0)
+  ])
+  let encryptedValue = cipher.update(paddedStr).toString('base64').replace(/\+/g, '-')
+  cipher.final()
+  return encryptedValue
+}
+encryptValue('testing')
